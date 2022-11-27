37408f877817b4fea224fec80526df94a5052a42
amrin.long <programrin2020@gmail.com>
2022-11-27 13:45:56 -0800

    completed challege 3

--- callbacks/challenges.js
+++ callbacks/challenges.js
@@ -1,13 +1,10 @@
-// Type JavaScript here and click "Run Code" or press Ctrl + s
 console.log('Hello, world!');
-
 
 // Challenge 1
 const addTwo = num => {
   return num + 2
 }
 
-// To check if you've completed it, uncomment these console.logs!
 console.log(addTwo(3));
 console.log(addTwo(10));
 
@@ -17,17 +14,16 @@ const addS = word => {
   return word + 's'
 }
 
-// uncomment these to check your work
 console.log(addS('pizza'));
 console.log(addS('bagel'));
 
 
 // Challenge 3
 function map(array, callback) {
-
+  return array.map(x => callback(x))
 }
 
-// console.log(map([1, 2, 3], addTwo));
+console.log(map([1, 2, 3], addTwo));
 
 
 // Challenge 4
