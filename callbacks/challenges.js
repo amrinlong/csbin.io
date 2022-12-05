00ceee7aaabde887213297ddeee243b2fba040de
amrin.long <programrin2020@gmail.com>
2022-12-04 17:28:36 -0800

    attemping intersection

--- callbacks/challenges.js
+++ callbacks/challenges.js
@@ -97,12 +97,14 @@ const intersection = arrays => {
   for (let i = 0; i < arrays.length; i++) {
     for (let j = 0; j < arrays[i].length; j++) {
       obj[arrays[i][j]] = +1
-      console.log(obj)
+      
     }
   }
+  console.log(Object.keys(obj))
+  return results
 }
 
-// console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
+console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
 // should log: [5, 15]
 
 
