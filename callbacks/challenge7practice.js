2e8eafda1c8e679785cab945ad6e9f11643bfb2b
amrin.long <programrin2020@gmail.com>
2022-12-05 20:11:28 -0800

    changes made on desktop

---
+++ callbacks/challenge7practice.js
@@ -0,0 +1,14 @@
+// - ### Challenge 7
+
+  // Construct a function `intersection` that takes in an array of arrays, compares the inner arrays, and returns a new array with elements found in all of them. BONUS: Use reduce!
+
+const intersection = arrays => {
+  let results = []
+  let obj = {}
+
+  
+
+}
+
+console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
+// should log: [5, 15]
