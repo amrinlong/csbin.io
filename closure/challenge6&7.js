3ac7895f94070516a4010cf5520609499ffc3b53
amrinlong <programrin2020@gmail.com>
2022-12-11 19:50:36 -0800

    completed challenge 6 and 7 on own

--- closure/challenge6&7.js
+++ closure/challenge6&7.js
@@ -1,18 +1,28 @@
 // CHALLENGE 6
-const delay = (cb, wait) => { 
-  
-}
+// const delay = (cb, wait) => { 
+//   return () => {
+//     return setTimeout(cb, wait)
+//   }
+// }
 
-let count = 0;
-const delayedFunc = delay(() => count++, 1000); 
-delayedFunc()
-console.log(count)                          // should print '0'
-setTimeout(() => console.log(count), 1000)  // should print '1' after 1 second                  
+// let count = 0;
+// const delayedFunc = delay(() => count++, 1000); 
+// delayedFunc()
+// console.log(count)                          // should print '0'
+// setTimeout(() => console.log(count), 1000)  // should print '1' after 1 second                  
 
 
 // CHALLENGE 7
 const rollCall = names => {
-  
+let count = 0;
+  return  () => {
+    if (count >= names.length) {
+        return console.log("Everyone accounted for")
+    }
+    
+    console.log(names[count])
+    return count++
+  }
 }
 
 const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
