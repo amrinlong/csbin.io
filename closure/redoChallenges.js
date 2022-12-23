79390e58e635a2c7d5fc15ea687e1df42a7622c9
amrinlong <programrin2020@gmail.com>
2022-12-22 18:26:08 -0800

    testing changes

--- closure/redoChallenges.js
+++ closure/redoChallenges.js
@@ -34,7 +34,6 @@ const outer = () => {
 const willCounter = outer();
 const jasCounter = outer();
 
-// Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
 
 // willCounter();
