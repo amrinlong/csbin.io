9fbb5c82de709f78eacbffa946dfed3a13b42e46
amrin.long <programrin2020@gmail.com>
2023-01-04 21:09:18 -0800

    completed challenge 3

--- recursion/challenges.js
+++ recursion/challenges.js
@@ -28,26 +28,26 @@ const sum = arr => {
 
 
 // Challenge 3
-const palindrome = (str) => {
-  // let strNoSpecChar = str.replace(/[^a-zA-Z ]/g, "")
+const palindrome = str => {
+  
+  let newStr = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, "")
+  let firstChar = newStr[0]
+  let lastChar = newStr.slice(-1)
 
-
-  let halfCount = strNoSpecChar.length / 2
-  //if odd or num ends in .5 math.floor drop to 
-
-
-
-  // console.log(halfCount)
-  // console.log(strNoSpecChar)
-   
+  if (newStr.length <= 1) {
+    return console.log('true')
+  } else {
+    if (firstChar === lastChar) {
+      palindrome(newStr.slice(1, -1))
+    } else {
+      return console.log('false')
+    }
+  }
 }
 
-console.log(palindrome("Ana"));
-console.log(palindrome("An-a"));
-console.log(palindrome("Anna"));
-console.log(palindrome("Anne, I vote more cars race Rome-to-Vienna")); //-> true
-console.log(palindrome("llama mall")); //-> true
-console.log(palindrome("jmoney")); //-> false
+// console.log(palindrome("Anne, I vote more cars race Rome-to-Vienna")); //-> true
+// console.log(palindrome("llama mall")); //-> true
+// console.log(palindrome("jmoney")); //-> false
 
 
 // Challenge 4
--- recursion/recursivePalindrome.js
+++ recursion/recursivePalindrome.js
@@ -1,6 +1,7 @@
 // Challenge 3
-const palindrome = (str) => {
-  let newStr = str.toLowerCase()
+const palindrome = str => {
+  
+  let newStr = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").replace(/\s/g, "")
   let firstChar = newStr[0]
   let lastChar = newStr.slice(-1)
 
@@ -8,18 +9,16 @@ const palindrome = (str) => {
     return console.log('true')
   } else {
     if (firstChar === lastChar) {
-      // console.log(str)
-      return palindrome(newStr.slice(1, -1))
+      palindrome(newStr.slice(1, -1))
+    } else {
+      return console.log('false')
     }
   }
-
-  return console.log('false')
-  // console.log(newStr.slice(1, -1))
 }
 
-console.log(palindrome("Janahj"));
-// console.log(palindrome("An-a"));
-// console.log(palindrome("Anna"));
-// console.log(palindrome("Anne, I vote more cars race Rome-to-Vienna")); //-> true
-// console.log(palindrome("llama mall")); //-> true
-// console.log(palindrome("jmoney")); //-> false
+console.log(palindrome("Janaj"));
+console.log(palindrome("An-a"));
+console.log(palindrome("Anna"));
+console.log(palindrome("Anne, I vote more cars race Rome-to-Vienna")); //-> true
+console.log(palindrome("llama mall")); //-> true
+console.log(palindrome("jmoney")); //-> false
