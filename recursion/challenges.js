c5f644fc782ef964c2de73a12ced756096b245ac
amrin.long <programrin2020@gmail.com>
2023-01-13 21:33:01 -0800

    need to loop each key in obj

--- recursion/challenges.js
+++ recursion/challenges.js
@@ -74,18 +74,20 @@ const isPrime = (num, i = num - 1) => {
 //Challenge 5
 const pathFinder = (obj, arr, total = Object.keys(obj).length) => { // total = 2
   if (arr.length === 1) {
-    console.log(obj[key])
+    console.log(obj[0])
   }
 
-  if (arr.length > 1) {
-    if (arr[0] === Object.keys(obj)[0]) { // or if Object.keys(obj)[0] === undefined
-      objOut = Object.values(obj)[0]
-      // console.log(Object.keys(obj)[0]) // ==> 'first'
-      // console.log(Object.values(obj)[0]) // ==> { second: { third: "finish" }}
-      newArr = arr.shift()
-      return pathFinder(objOut, newArr)
+  for (let keys in obj) {
+    if (arr.length > 1) {
+      if (arr[0] === Object.keys(obj)[0]) {
+        objOut = Object.values(obj)[0]
+        arr.shift()
+        return pathFinder(objOut, newArr)
+      }
     }
   }
+
+  
 
 }
 
--- recursion/pathfinder.js
+++ recursion/pathfinder.js
@@ -7,14 +7,28 @@ const pathFinder = (obj, arr) => { // Object.keys(obj).length = 2
         console.log(firstObjVal)
     }
 
-    if (arr[0] === firstObjKey) {
-        arr.shift()
-        return pathFinder(firstObjVal, arr)
-    }
+    // for (let key in obj) {
+    //     if (arr[0] !== key) {
+    //         continue
+    //     }
+        
+    //     if (arr[0] === firstObjKey) {
+    //     arr.shift()
+    //     return pathFinder(firstObjVal, arr)
+    //     }
+
+    // }
+
+    // if (arr[0] === firstObjKey) {
+    //     arr.shift()
+    //     return pathFinder(firstObjVal, arr)
+    // } 
+
+    console.log(firstObjVal)
 }
 
 const obj = { first: { second: { third: "finish" } }, 
                 second: { third: "wrong" } };
-const arr = ["first", "second", "third"];
-// const arr = ["second", "third"];
+// const arr = ["first", "second", "third"];
+const arr = ["second", "third"];
 console.log(pathFinder(obj, arr));   //-> "finish"
