de2cc806764845dea90e17d83bca31ae1cae82f0
amrinlong <programrin2020@gmail.com>
2023-01-13 20:47:46 -0800

    finish for first test case but not second for pathfinder

--- recursion/pathfinder.js
+++ recursion/pathfinder.js
@@ -1,23 +1,20 @@
 //Challenge 5
-const pathFinder = (obj, arr, total = Object.keys(obj).length) => { // total = 2
+const pathFinder = (obj, arr) => { // Object.keys(obj).length = 2
+    const firstObjVal = Object.values(obj)[0]
+    const firstObjKey = Object.keys(obj)[0]
+
     if (arr.length === 1) {
-        console.log(obj[key])
+        console.log(firstObjVal)
     }
 
-    if (arr.length > 1) {
-      if (arr[0] === Object.keys(obj)[0]) { // or if Object.keys(obj)[0] === undefined
-        objOut = Object.values(obj)[0]
-        // console.log(Object.keys(obj)[0]) // ==> 'first'
-        // console.log(Object.values(obj)[0]) // ==> { second: { third: "finish" }}
-        newArr = arr.shift()
-        return pathFinder(objOut, newArr)
-        }
+    if (arr[0] === firstObjKey) {
+        arr.shift()
+        return pathFinder(firstObjVal, arr)
     }
-
 }
 
 const obj = { first: { second: { third: "finish" } }, 
                 second: { third: "wrong" } };
 const arr = ["first", "second", "third"];
-  // const arr = ["second", "third"];
-  console.log(pathFinder(obj, arr));   //-> "finish"
+// const arr = ["second", "third"];
+console.log(pathFinder(obj, arr));   //-> "finish"
