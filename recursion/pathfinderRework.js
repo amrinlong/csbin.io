583354b24b97dec42d7712f21a64c9c31bb26244
amrin.long <programrin2020@gmail.com>
2023-01-13 22:06:15 -0800

    added rework for pathfinder

---
+++ recursion/pathfinderRework.js
@@ -0,0 +1,20 @@
+const pathFinder = (obj, arr) => { // Object.keys(obj).length = 2
+  const firstObjVal = Object.values(obj)[0]
+  const firstObjKey = Object.keys(obj)[0]
+
+  if (arr.length === 1) {
+      console.log(firstObjVal)
+  }
+
+  for (let key in obj) {
+    if (key === arr[0]) {
+      
+    }
+  }
+}
+
+const obj = { first: { second: { third: "finish" } }, 
+              second: { third: "wrong" } };
+// const arr = ["first", "second", "third"];
+const arr = ["second", "third"];
+console.log(pathFinder(obj, arr));   //-> "finish"
